c25f5000ebbbf749b281aee34534ceff4de91018
ARA34 <agreyesaranda@gmail.com>
2025-04-02 16:38:24 -0700

    Invasive Species bug fix

--- bundle.js
+++ bundle.js
@@ -2387,19 +2387,28 @@ module.exports = class {
         this.initReqs();
         if (!is(fileObj)) return;
 
+        let spriteLibrary = []; // not in use
+
         let stage = project.targets.find(t=>t.isStage);
         let sprites = project.targets.filter(t=>!t.isStage);
-        let invasiveSprite = project.targets.find(t=>t.name.toLowerCase() == "invasive");
-        let explainerSprite = project.targets.find(t=>t.name.toLowerCase() == "sprite");
 
-        this.requirements.invasiveSprite.bool = invasiveSprite != null; //NUANCE: sprite must be named invasive
-        this.requirements.invasiveExplained.bool = (invasiveSprite != null) ? invasiveSprite.scripts.some(s=>s.blocks[0].opcode.includes("event_") && s.blocks.some(b=>b.opcode.includes("looks_say") || b.opcode.includes("sound_play"))): false; //NUANCE: Sprite must be named invasive
-        this.requirements.ecosystemExplained.bool = (explainerSprite != null) ? explainerSprite.scripts.some(s=>s.blocks[0].opcode.includes("event_") && s.blocks.some(b=>b.opcode.includes("looks_say") || b.opcode.includes("sound_play"))): false; //NUANCE: Sprite must be named sprite
-        this.requirements.backdropPresent.bool = stage.costumes.length >= 1; //NUANCE: can't tell that the image is of ecosystem
+        function procSprite(sprite) {
+            var out = { valid: false };
+
+            out.valid = sprite.scripts.some(s=>s.blocks[0].opcode.includes("event_") && s.blocks.some(b=>b.opcode.includes("looks_say") || b.opcode.includes("sound_play")));
+
+            return out;
+        }
+        var results = sprites.map(procSprite);
+
+        this.requirements.invasiveSprite.bool = sprites.length >= 1; // NUANCE: cannot tell uploaded
+        this.requirements.invasiveExplained.bool = results.filter(o=>o.valid).length >= 2;
+        this.requirements.ecosystemExplained.bool = results.filter(o=>o.valid).length >= 2;
+        this.requirements.backdropPresent.bool = stage.costumes.length >= 1;
         
         console.log("-- DEBUG --");
-        console.log("IMPORTANT: Invasive sprite must be named 'invasive'");
-        console.log("IMPORTANT: Explainer sprite must be named 'sprite'");
+        // console.log("IMPORTANT: Invasive sprite must be named 'invasive'");
+        // console.log("IMPORTANT: Explainer sprite must be named 'sprite'");
 
         return;
     }
